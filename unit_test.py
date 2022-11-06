b2d1dc3b6c20182d42aa0399697172e6418a1006
satvik333 <55409095+satvik333@users.noreply.github.com>
2022-11-06 21:02:02 +0530

    Update unit_test.py

--- unit_test.py
+++ unit_test.py
@@ -1,16 +1,23 @@
 noOfInputs = int(input("Enter num of elements"))
 arr = []
+newArr = []
 
 for i in range(noOfInputs):
-    a = int(input())
+    a = int(input('Enter the element'))
     arr.append(a)
 
 findingNo = int(input("Enter the num to find"))
 
 def findTheNumber(arr, findingNo):
+    arr = arr.sort()
     for i in arr:
         if(i == findingNo):
-            return (i)
+            return i
+        else:
+            return arr[min(range(len(arr)), key = lambda i: abs(arr[i]-findingNo))]
+      
+
+
 
 findTheNumber(arr,findingNo)
 
@@ -19,8 +26,13 @@ import unittest
 class TestStringMethods(unittest.TestCase):
 
     def test_finding(self):
-        a = findTheNumber([1,2,3],3)
-        self.assertEqual(a, 3)
+        c = findTheNumber([1,2,3],2)
+        self.assertEqual(c, 2)
+
+    def test_notFinding(self):
+        d = findTheNumber([1,2,5],3)
+        self.assertEqual(d, 2)
+
 
 if __name__ == '__main__':
     unittest.main()
